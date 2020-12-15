7fc28dbb46db4d504580981a9ef0a517c585c889
Yaowei Wang <43977524+ywwang0@users.noreply.github.com>
2020-12-15 21:11:49 +0800

    Update seperate_number_and_str.py

--- seperate_number_and_str.py
+++ seperate_number_and_str.py
@@ -3,8 +3,10 @@ import re
 
 df = pd.read_excel('binary.xlsx')
 for n,i in enumerate(list(df['Formula'])):
+    # 判断化学式中有没有数字
     if len(re.findall(r"\d+\.?\d*",i)) == 0:
         df.loc[n,'if natoms greater than 30'] = 0
+    # 最大的数字小于30就是没超过30
     elif eval(max(re.findall(r"\d+\.?\d*",i))) < 30:
         df.loc[n,'if natoms greater than 30'] = 0
     else:
